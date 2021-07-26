8e6149cdbbe23b152976f08b1beef4fce95a160b
contacto@belucky.cl <contacto@belucky.cl>
2021-07-26 16:35:05 +0000

    2 commit

--- bw_maqueta/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ bw_maqueta/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -3,8 +3,7 @@
     'name': "bw_maqueta",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Moódulo que permite habilitar maqueta de Bulk Wine""",
 
     'description': """
         Long description of module's purpose
